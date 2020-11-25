e2452726ee81b300347a7ade8cc47b87df5ed790
Sean Smith <40581796+smithlabs@users.noreply.github.com>
2020-11-24 20:57:42 -0700

    Output

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,11 @@ output "testing_sclient" {
   value = "Test this SSL by using openssl s_client -host ${aws_route53_record.myapp.fqdn} -port 443 and looking at the certs"
 }
 
+output "zone_public_id"
+  description = "The public ID of the DNS zone"
+  value = "data.aws_route53_zone.public.id"
+}
+
 #output "acm_arn" {
 #  value = aws_acm_certificate_validation.cert.certificate_arn
 #}
